9550cedcfbcc5c0eb1be3b223f5d51d70fb08735
anderson-arc <romanrozin2m2@gmail.com>
2020-06-20 18:04:24 -0400

    git dump functionality

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+import org.gradle.jvm.tasks.Jar
+
+plugins {
+    java
+    kotlin("jvm") version "1.3.72"
+}
+
+group = "com.github.andersonarc"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks {
+    register<Jar>("buildJar") {
+        dependsOn(configurations.compileClasspath)
+        manifest {
+            attributes(mapOf("Main-Class" to "MainKt"))
+        }
+        from(sourceSets.main.get().output)
+        from(configurations.compileClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) })
+    }
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
